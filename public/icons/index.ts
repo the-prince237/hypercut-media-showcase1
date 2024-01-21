0c0ec3afaebba1060532cc38c49b09b19aa7b2d1
= <=>
2024-01-21 18:37:12 +0100

    adding sm icons exports

---
+++ public/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as facebookIcon } from './facebook.svg';
+export { default as instagramIcon } from './instagram.svg';
+export { default as messengerIcon } from './messenger.svg';
+export { default as whatsappIcon } from './whatsapp.svg';
+export { default as gmailIcon } from './gmail.svg';
