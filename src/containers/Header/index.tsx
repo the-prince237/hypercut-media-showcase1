bf9ffc27aeea3b0a1abc57be11bbecaaaffa561a
= <=>
2024-01-23 12:45:19 +0100

    responsiveness done

--- src/containers/Header/index.tsx
+++ src/containers/Header/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Box } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Logo } from '@/components';
 import { MenuNavigator } from '../../components/MenuNavigator';
 import Image from 'next/image';
@@ -10,21 +10,39 @@ import classNames from 'classnames';
 
 const Header = () => {
   const [menuOpened, setMenuOpened] = useState(false);
+  // header bg state
+  const [bg, setBg] = useState(false);
+  //nav mobile state
+  const [navMobile, setNavMobile] = useState(false);
+
+  //scroll event
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      window.scrollY > 800 ? setBg(true) : setBg(false);
+    });
+  });
+
   return (
-    <Box className='largeTablet:relative fixed z-10 flex h-[70px] w-full items-center justify-between px-[45px] tablet:h-[100px] tablet:px-[64px] minLaptop:h-[150px] laptopView:px-[90px]'>
+    <Box
+      className={classNames(
+        'largeTablet:relative z-10 flex h-[80px] w-full items-center justify-between px-[45px] tablet:h-[100px] tablet:px-[64px] minLaptop:h-[150px] laptopView:px-[90px]',
+        { 'largeTablet:relative fixed bg-boxBlueTrans backdrop-blur-lg': bg },
+      )}
+    >
       <Logo />
       <Box className='largeTablet:block hidden'>
         <MenuNavigator />
       </Box>
       {menuOpened && (
-        <Box className='largeTablet:hidden animate-wobble fixed left-0 top-0 flex h-screen w-screen items-center justify-center bg-boxBlueTrans backdrop-blur-3xl'>
+        <Box className='largeTablet:hidden animate-wobble fixed left-0 top-0 flex h-screen w-screen items-center justify-center bg-boxBlue'>
           <MenuNavigator col />
         </Box>
       )}
       <Image
         src={menuIcon}
-        className={classNames('largeTablet:hidden relative rotate-[-180deg]', {
+        className={classNames('largeTablet:hidden relative', {
           'rotate-0': menuOpened,
+          'rotate-[-180deg]': !menuOpened,
         })}
         onClick={() => setMenuOpened((mo) => !mo)}
         alt='menu'
