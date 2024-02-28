84dc33e2a42c4b71284d4ee563193099bba951fe
= <=>
2024-02-28 14:50:01 +0100

    Adding facebook meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import './globals.css';
 import Providers from '@/providers';
-import { Footer, Header } from '@/containers';
+
 const poppins = Poppins({ weight: '400', subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -13,6 +13,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
+      <head>
+        <meta name='facebook-domain-verification' content='w99474bb4idskwrs6sg5cj9cnt6mj8' />
+      </head>
       <body className={poppins.className}>
         <Providers>{children}</Providers>
       </body>
