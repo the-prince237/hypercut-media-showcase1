eccd6b6fcc60bbfb7066ad0af36a0f7d015dc676
= <=>
2024-03-21 20:05:06 +0100

    analytics gtag installed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import './globals.css';
 import Providers from '@/providers';
+import { GoogleTagManager } from '@next/third-parties/google';
 
 const poppins = Poppins({ weight: '400', subsets: ['latin'] });
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang='en'>
       <head>
         <meta name='facebook-domain-verification' content='w99474bb4idskwrs6sg5cj9cnt6mj8' />
+        <GoogleTagManager gtmId='G-YQJR10W67D' />
       </head>
       <body className={poppins.className}>
         <Providers>{children}</Providers>
