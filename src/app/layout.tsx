a3e2c89fd9f5fb9f848f42cacd02bf6709975b31
= <=>
2024-06-10 02:36:17 +0100

    Add hotjar with react-hotjar package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,12 +16,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang='en'>
       <head>
         <meta name='facebook-domain-verification' content='w99474bb4idskwrs6sg5cj9cnt6mj8' />
-      </head>
-      <body className={poppins.className}>
-        <Providers>
-          {children}
-          <Script id='HotjarIntegration'>
-            {`(function(h,o,t,j,a,r){
+        <Script id='HotjarIntegration'>
+          {`(function(h,o,t,j,a,r){
                 h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
                 h._hjSettings={hjid:5017930,hjsv:6};
                 a=o.getElementsByTagName('head')[0];
@@ -29,8 +25,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
                 a.appendChild(r);
               })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
-          </Script>
-        </Providers>
+        </Script>
+      </head>
+      <body className={poppins.className}>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
